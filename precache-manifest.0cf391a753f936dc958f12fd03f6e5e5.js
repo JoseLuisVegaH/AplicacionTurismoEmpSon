31a6f6feb26ce46e213cc754870c25063550e0ba
JoseLuisVegaH <jlvh1996@gmail.com>
2020-02-28 00:38:50 -0700

    Updates

---
+++ precache-manifest.0cf391a753f936dc958f12fd03f6e5e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5a1900e77c0c0b6150f7a239d87177",
+    "url": "/Prototipo-WDev/index.html"
+  },
+  {
+    "revision": "071ffc61c1e9d0dcd2c9",
+    "url": "/Prototipo-WDev/static/css/main.bbfe3004.chunk.css"
+  },
+  {
+    "revision": "e13a26a35f2beac661ef",
+    "url": "/Prototipo-WDev/static/js/2.a8da2f8a.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Prototipo-WDev/static/js/2.a8da2f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "071ffc61c1e9d0dcd2c9",
+    "url": "/Prototipo-WDev/static/js/main.5fdff6af.chunk.js"
+  },
+  {
+    "revision": "0662dc065117e11ca34a",
+    "url": "/Prototipo-WDev/static/js/runtime-main.6bef7565.js"
+  },
+  {
+    "revision": "d86127cfd9930b16b735ea8989597b42",
+    "url": "/Prototipo-WDev/static/media/anuncio.d86127cf.jpg"
+  },
+  {
+    "revision": "48a077bad8e126035c39aaac9f5a518f",
+    "url": "/Prototipo-WDev/static/media/logo-pescado.48a077ba.png"
+  },
+  {
+    "revision": "dc90c56370539808012bd4dc7072fc9f",
+    "url": "/Prototipo-WDev/static/media/mount.dc90c563.jpg"
+  },
+  {
+    "revision": "e264cd7aa82734ce8a9f58e4bb10453e",
+    "url": "/Prototipo-WDev/static/media/top.e264cd7a.jpg"
+  }
+]);
